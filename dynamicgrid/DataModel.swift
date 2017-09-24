74e193897a678e9b45cb99098eef9c7276b152eb
abuzeid-timore <abuzeidtimore@gmail.com>
2017-09-24 18:36:40 +0200

    initial commit, creating hierarchy for MVP

---
+++ dynamicgrid/DataModel.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  dynamicgrid
+//
+//  Created by abuzeid on 9/24/17.
+//  Copyright © 2017 abuzeid. All rights reserved.
+//
+
+import UIKit
+
+struct  DataModel {
+    private var id:Int
+    private var title:String
+    private var width:Int
+   
+}
