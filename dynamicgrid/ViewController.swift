ed18f88d7cda6606404fb638764bd9a9dda7b59b
abuzeid-timore <abuzeidtimore@gmail.com>
2017-09-24 18:30:05 +0200

    Initial Commit

---
+++ dynamicgrid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamicgrid
+//
+//  Created by abuzeid on 9/24/17.
+//  Copyright © 2017 abuzeid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
